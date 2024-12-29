c2c5abdd8f806f4578b05959335d6ea178813087
Hashir Shahzad <hashir447079@gmail.com>
2024-12-29 08:57:54 +0500

    Testing lazygit

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
+// added a comment
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
     loop {
         terminal.draw(|f| ui(f, app))?;
